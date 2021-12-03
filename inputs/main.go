a09da94ec6da34e21a93342b1378fba1be6ca643
antti poutiainen <antti.poutiainen@polarsquad.com>
2021-12-03 08:14:48 +0200

    some changes for inputs

--- inputs/main.go
+++ inputs/main.go
@@ -56,3 +56,28 @@ func ReadInts(filepath string) []int {
 	}
 	return data
 }
+
+func ReadBytes(filepath string) []byte {
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	data := make([]byte, 0)
+
+	for scanner.Scan() {
+		i := scanner.Text()
+		if err != nil {
+			continue
+		}
+		data = append(data, i...)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
+}
